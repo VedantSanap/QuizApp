b01ea99b08df252f3591b054dcf12efb48606d27
VedantSanap <91800562+VedantSanap@users.noreply.github.com>
2023-03-02 23:21:55 +0530

    Create question.js

---
+++ question.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following attribute is used for merging two or more adjacent columns?",
+    answer: "COLSPAN",
+    options: [
+      "CELLPADDING",
+      "ROWSPAN",
+      "CELLSPACING",
+      "COLSPAN"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What keyword is used to declare an asynchronous function in Javascript?",
+    answer: "async",
+    options: [
+      "async",
+      "await",
+      "setTimeout",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the general syntax of writing the var() function?",
+    answer: "var(--name,value)",
+    options: [
+      "var(--name,value)",
+      "var(--name)",
+      "var(value)",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  }
+];
